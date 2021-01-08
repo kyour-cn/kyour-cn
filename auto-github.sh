8f97162e6fd9c9c52097f92250d835db9451f552
kyour <kyour@vip.qq.com>
2021-01-08 16:21:58 +0800

    sign-in

---
+++ auto-github.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+php auto.php
+
+git add .
+
+git commit -m"sign-in"
+
+git push
---
+++ auto.php
@@ -0,0 +1,4 @@
+<?php
+
+file_put_contents("./sign-in/log-".date('ymd').".txt", "Sign in today. ".date('Y-m-d'));
+
