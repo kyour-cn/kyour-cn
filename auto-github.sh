4f86e5d8cb6e19ba3e6b07c25a5c0bb08001e5cf
kyour <kyour@vip.qq.com>
2021-01-08 17:05:34 +0800

    sign-in

--- auto-github.sh
+++ auto-github.sh
@@ -1,5 +1,12 @@
 #!/bin/bash
 
+# /data/dev/kyour-cn/auto-github.sh
+
+# vim /var/spool/cron/root
+# systemctl restart crond
+
+git pull
+
 php auto.php
 
 git add .
---
+++ sign-in/2021-01/today-08.md
@@ -0,0 +1,8 @@
+
+# To day 2021-01-08
+
+
+## 如此渺小的我们，只有通过爱，才能承受宇宙的广漠。
+> For small creatures such as we the vastness is bearable only through love.
+
+    
