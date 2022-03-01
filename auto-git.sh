410dae5a27cec81821ffe66db2fefd2dbf752175
kyour-cn <kyour@vip.qq.com>
2022-03-01 10:33:14 +0800

    sign-in

--- auto-git.sh
+++ auto-git.sh
@@ -1,20 +1,3 @@
 #!/bin/bash
 
-# /data/dev/kyour-cn/auto-git.sh
-
-# vim /var/spool/cron/root
-# systemctl restart crond
-
-cd /data/script/git/kyour-cn
-
-git pull
-
-php auto.php
-
-git add .
-
-git commit -m"sign-in"
-
-git push
-
-cd -
+php /data/script/git/kyour-cn/auto.php
